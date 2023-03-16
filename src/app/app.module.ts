aa13d9fdd016e9aabc9da94f8340111a0238335e
Mulder8vidas <mzutam@autonoma.edu.pe>
2023-03-15 19:50:10 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Pages/header/header.component';
+import { BodyComponent } from './Pages/body/body.component';
+import { FooterComponent } from './Pages/footer/footer.component';
+import {RouterModule, Routes} from "@angular/router";
+import {NgxTypedJsModule} from "ngx-typed-js";
+
+const appRoutes:Routes=[
+
+  {path:'',component:BodyComponent  }
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+      NgxTypedJsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
